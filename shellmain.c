91cad07188986d2a408ff6b439de665cbb550088
Zevrov <719@holbertonschool.com>
2019-04-10 22:45:13 +0000

    update to shellmain.c

--- shellmain.c
+++ shellmain.c
@@ -8,11 +8,12 @@
 
 int main(int argc, char *argv[])
 {
-	char *buffer;
+	char *buffer = NULL;
 	size_t buffsize = 0;
 	pid_t newpid;
 	ssize_t charCount;
 	int status;
+	char **cmd;
 
 	if (argc < 1)
 		return (-1);
@@ -25,12 +26,13 @@ int main(int argc, char *argv[])
 			break;
 		if (buffer[charCount - 1] == '\n')
 			buffer[charCount - 1] = '\0';
+		cmd = vect_array(buffer, charCount);
 		newpid = fork();
 		if (newpid < 0)
 			perror(argv[0]);
 		if (newpid == 0)
 		{
-			execve(buffer, argv, NULL);
+			execve(cmd[0], cmd, envp);
 			perror(argv[0]);
 		}
 		else
@@ -39,5 +41,6 @@ int main(int argc, char *argv[])
 	if (charCount < 0)
 		write(STDERR_FILENO, "\n", 1);
 	free(buffer);
+	free(cmd);
 	return (0);
 }
