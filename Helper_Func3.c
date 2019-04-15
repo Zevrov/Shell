4bb5c8ef0301db43dedcb6de1ef82034c24cee8f
DracoMindz <704@holbertonschool.com>
2019-04-15 14:53:50 +0000

    Helper Functions

--- Helper_Func3.c
+++ Helper_Func3.c
@@ -49,3 +49,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	return (arr);
 }
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
